7a5ae7aabf09843d58ca9370efd88f1080e25b4f
gabrielaramburu <gabrielaramburu@gmail.com>
2021-07-07 22:48:00 -0300

    final version that pass all test

--- src/tree/binarytree/rotation/Node.java
+++ src/tree/binarytree/rotation/Node.java
@@ -19,13 +19,40 @@ public class Node {
 	}
 	
 	
-    public Node rotateRight(Node root) {
-        return root; // Replace with your solution
+    public static Node rotateRight(Node root) {
+    	if (root.left == null) return root;
+    	Node oldRoot = root;
+    	root = root.left;
+    	Node auxRight = root.right;
+    	root.right = oldRoot;
+    	oldRoot.left = auxRight;
+        return root;
     }
 
-    public Node rotateLeft(Node root) {
-        return root; // Replace with your solution
+    public static Node rotateLeft(Node root) {
+    	if (root.right == null) return root;
+    	Node oldRoot = root;
+    	root = root.right;
+    	Node auxLeft = root.left;
+    	root.left = oldRoot;
+    	oldRoot.right = auxLeft;
+        return root; 
     }
    
+    
+    @Override
+	public boolean equals(Object obj) {
+    	if (obj != null) return this.value == ((Node)obj).value;
+    	else return false;
+	}
+
+	public static void showPreOrder(Node node) {
+		if (node != null) {
+			System.out.print(node.value + ", ");
+			Node.showPreOrder(node.left);
+			Node.showPreOrder(node.right);
+		}
+	}
 	
+    
 }
--- src/tree/binarytree/rotation/RotationTest.java
+++ src/tree/binarytree/rotation/RotationTest.java
@@ -1,6 +1,7 @@
 package tree.binarytree.rotation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class RotationTest {
@@ -21,7 +22,18 @@ public class RotationTest {
                                       .setRight(new Node(8)
                                                 .setLeft(new Node(7))
                                                 .setRight(new Node(12))));
-        assertEquals(initialNode.rotateRight(initialNode), (resultNode));
+        
+        
+        System.out.println("Before right rotate:");
+        Node.showPreOrder(initialNode);
+        System.out.println(" ");
+        
+        Node result = Node.rotateRight(initialNode);
+        
+        System.out.println("After right rotate:");
+        Node.showPreOrder(result);
+        System.out.println(" ");
+        assertEquals(result, (resultNode));
                            
     }
 
@@ -31,7 +43,45 @@ public class RotationTest {
                           .setLeft(new Node(5)
                                       .setLeft(new Node(2))
                                       .setRight(new Node(7)))
-                          .setRight(new Node(12));                                      
-        assertEquals(initialNode.rotateLeft(initialNode), (resultNode));
+                          .setRight(new Node(12));               
+        
+        System.out.println("Before left rotate:");
+        Node.showPreOrder(initialNode);
+        System.out.println(" ");
+        Node result = Node.rotateLeft(initialNode);
+        
+        System.out.println("After left rotate:");
+        Node.showPreOrder(result);
+        System.out.println(" ");
+        assertEquals(result, (resultNode));
+    }
+    
+    @Test
+    public void unrotatableNodeRight() throws Exception {
+    	  Node unrotableNode = new Node(5)
+                  .			setRight(new Node(6)
+                              .setRight(new Node(7)));
+
+                                      
+        
+        System.out.println("unrotable, before right rotate:");
+        Node.showPreOrder(unrotableNode);
+        System.out.println("");
+        Node result = Node.rotateRight(unrotableNode);
+        System.out.println("unrotable, after right rotate:");
+        Node.showPreOrder(result);
+        
+        assertEquals(result, (unrotableNode));
+    }
+    
+    @Test
+    public void unrotatableNodeLeft() throws Exception {
+        Node unrotableNode = new Node(5)
+                          .setLeft(new Node(4)
+                                      .setLeft(new Node(3)));
+        
+       
+        Node result = Node.rotateLeft(unrotableNode);
+        assertEquals(result, (unrotableNode));
     }
 }
