cfd66f6c4b7f1c35108abe3407bf0a0a82b95372
gabrielaramburu <gabrielaramburu@gmail.com>
2021-07-08 23:58:35 -0300

    final version that pass all test

--- src/tree/binarytree/complete/CompleteBinaryTreeFromArray.java
+++ src/tree/binarytree/complete/CompleteBinaryTreeFromArray.java
@@ -3,6 +3,7 @@ package tree.binarytree.complete;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class CompleteBinaryTreeFromArray {
 	
@@ -14,22 +15,37 @@ class CompleteBinaryTreeFromArray {
 		int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		
 		int[] result = new CompleteBinaryTreeFromArray().apply(input);
-		System.out.println(" resultado: " + result.toString());
-
+		System.out.println("Result: " + Arrays.toString(result));
 	}
 
 	public int[] apply(int[] input) {
 		Integer[] inputAsInteger = completeMissingNodes(input);
+		
 		Node root = buildTree(inputAsInteger);
 		traverseBFS(root, inputAsInteger.length);
 		return output.stream().filter(item -> item != null?true:false).mapToInt(i -> i).toArray();
 	}
 
+	/* Transform the input array in a Perfect Binary tree representation */ 
 	private Integer[] completeMissingNodes(int[] input) {
-		int expectedNodes = input
-		return null;
+		List<Integer> perfectTree = Arrays.stream(input).boxed().collect(Collectors.toList());
+		
+		int treeLevel = treeLevel(input.length);
+		int expectedNodes = (int)Math.pow(2, treeLevel + 1) - 1;
+		
+		int missingNodes = expectedNodes - input.length;
+		for (int i=perfectTree.size(); i >=0; i--) {
+			if (missingNodes>0) {
+				perfectTree.add(i, null);
+				missingNodes--;
+			} else break;
+		}
+		System.out.println("conversion.");
+		perfectTree.stream().forEach(val -> System.out.print(val + ", "));
+		return perfectTree.toArray(Integer[]::new);
 	}
 
+	/* The input array follows the pattern: left parent right  */
 	private Node buildTree(Integer[] input) {
 		if (input.length == 1) 
 			return new Node(input[0]);
@@ -65,9 +81,12 @@ class CompleteBinaryTreeFromArray {
 		return leafs;
 	}
 	
+	private int treeLevel (int cantNodes) {
+		return (int)(Math.log(cantNodes) / Math.log(2));
+	}
+	
 	private void traverseBFS(Node root, int cantNodes) {
-		int leves = (int)(Math.log(cantNodes) / Math.log(2));
-		for (int level=1;level<=leves;level++) 
+		for (int level=1;level<=treeLevel(cantNodes) + 1;level++) 
 			showLevel(root, level);
 	}
 
