35d9d89f991e2489e93109a060f6820caa0827c1
gabrielaramburu <gabrielaramburu@gmail.com>
2021-07-23 00:22:18 -0300

    some aditional performance testing on quicksort implementation

--- src/sorting/quicksort/Testing.java
+++ src/sorting/quicksort/Testing.java
@@ -3,29 +3,56 @@ package sorting.quicksort;
 import java.util.Arrays;
 import java.util.Random;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class Testing {
+	//NOTE: if the array is already sorted the v1 and v2 implementations throws
+	//a StackOverflow exception when the arrays is around 10_000 length
 	private static final int TEST_SIZE = 5000;
-	static int[] input = new int[TEST_SIZE];
 	
-	@BeforeAll
-	public static void initialize() {
-		Arrays.stream(input).forEach(item -> item =  new Random().nextInt(TEST_SIZE));
-	}
 	@Test
 	void testPerformance() {
+		int[] arr1 = new int[TEST_SIZE];
+		for (int i=0; i<arr1.length;i++) arr1[i] = new Random().nextInt(TEST_SIZE);
+			
+		System.out.println("Start sorting...");
 		long ini = System.currentTimeMillis();
-		new QuicksortVersion1().quickSort(input);
+		Arrays.sort(arr1);
+		System.out.println("Execution time Java impletentation = " + (System.currentTimeMillis() - ini));
+	
+		int[] arr2 = new int[TEST_SIZE];
+		for (int i=0; i<arr2.length;i++) arr2[i] = new Random().nextInt(TEST_SIZE);
+		ini = System.currentTimeMillis();
+		new QuicksortVersion1().quickSort(arr2);
 		System.out.println("Execution time v1 = " + (System.currentTimeMillis() - ini));
 		
+		int[] arr3 = new int[TEST_SIZE];
+		for (int i=0; i<arr3.length;i++) arr3[i] = new Random().nextInt(TEST_SIZE);
 		ini = System.currentTimeMillis();
-		new QuicksortVersion2().quickSort(input);
+		new QuicksortVersion2().quickSort(arr3);
 		System.out.println("Execution time v2 = " + (System.currentTimeMillis() - ini));
+	}	
 		
-		ini = System.currentTimeMillis();
-		Arrays.sort(input);
+	@Test
+	void testAlreadySortedArrays() {
+		int[] arr1 = new int[TEST_SIZE];
+		for (int i=0; i<arr1.length;i++) arr1[i] = i;
+			
+		System.out.println("Start sorting...");
+		long ini = System.currentTimeMillis();
+		Arrays.sort(arr1);
 		System.out.println("Execution time Java impletentation = " + (System.currentTimeMillis() - ini));
-	}
+	
+		int[] arr2 = new int[TEST_SIZE];
+		for (int i=0; i<arr2.length;i++) arr2[i] = i;
+		ini = System.currentTimeMillis();
+		//new QuicksortVersion1().quickSort(arr2);
+		System.out.println("Execution time v1 = " + (System.currentTimeMillis() - ini));
+		
+		int[] arr3 = new int[TEST_SIZE];
+		for (int i=0; i<arr3.length;i++) arr3[i] = i;
+		ini = System.currentTimeMillis();
+		new QuicksortVersion2().quickSort(arr3);
+		System.out.println("Execution time v2 = " + (System.currentTimeMillis() - ini));
+	}	
 }
