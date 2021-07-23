6deb3459e89a1c32e8c2bbbf58f477e8249b6905
gabrielaramburu <gabrielaramburu@gmail.com>
2021-07-23 20:22:11 -0300

    fibonacci recursive implementation

---
+++ src/recursion/Fibonacci.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class Fibonacci {
+
+	//1,1,2,3,5,8,13
+	public static void main(String[] args) {
+		Fibonacci fibo = new Fibonacci();
+		System.out.println(fibo.fibonacci(40));
+	}
+
+	private int fibonacci(int i) {
+		return fibonacci(0,1, 40);
+	}
+	
+	private int fibonacci(int ant, int sig, int max) {
+		if (ant > max) return sig;
+		
+		System.out.println(ant);
+		return fibonacci(ant + sig, ant, max);
+	}
+
+}
